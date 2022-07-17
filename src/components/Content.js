108f11a2828e53e8ca0aee1a860144a25b0cbf2d
Michelle <mweng@buffalo.edu>
2022-07-16 23:55:42 -0400

    update about me

--- src/components/Content.js
+++ src/components/Content.js
@@ -76,7 +76,7 @@ function Content() {
               
             <div className='projecto'>
                   {/* Project 1  */}
-                <a href="https://streamdigitaltwin.netlify.app/" aria-label="GitHub" target="_blank" rel="noreferrer">
+                <a href="http://44.206.189.200:3000/" aria-label="GitHub" target="_blank" rel="noreferrer">
                     <h2 className='center san project__tit'>Current Project for REU</h2>
                 </a>
                 <StaticImage 
@@ -103,7 +103,7 @@ function Content() {
                             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                             </svg>
                         </a> */}
-                        <a href="https://streamdigitaltwin.netlify.app/" aria-label="WebSite" target="_blank" rel="noreferrer">
+                        <a href="http://44.206.189.200:3000/" aria-label="WebSite" target="_blank" rel="noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg"
                             role="img"
                             viewBox="0 0 32 32" fill="none" stroke="currentColor"
@@ -292,9 +292,9 @@ function Content() {
 
         <section id='contact'>
             <h1 className='san yellow'>04. Hello there,</h1>
-              <p>I am currently a third year Computer Science major at University at Buffalo.</p>
-              <p>In my free time, I'm interested in following the developments of science. I also play a lot of video games.</p>
-              <p>If you're looking to connect to talk about experiences, education, connect about an opportunity, feel free to send me an email or connect on linkedin!</p>
+              <p>I am currently a rising third year Computer Science major at University at Buffalo.</p>
+              <p>In my free time, I enjoy watching cooking videos and learning about some new technology. </p>
+              <p>If you're looking to talk about experiences, education, connect about an opportunity, feel free to send me an email!</p>
         </section>
 
         <div id='social' className='footer__icons' >
